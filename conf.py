2173a14e9bda3f3417a71a335255d17cc1e77596
hd4niel <daniel.holmberg97@gmail.com>
2020-04-14 01:58:49 +0300

    Fix duck

--- conf.py
+++ conf.py
@@ -58,7 +58,7 @@ exclude_patterns = ['_build', '**.ipynb_checkpoints']
 #
 html_theme = 'sphinx_rtd_theme'
 html_logo = 'duck.png'
-html_favicon = 'ducks.png'
+html_favicon = 'duck.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
