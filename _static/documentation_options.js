6589a6295137b1a9167069aa2277c03ee9e452a0
hd4niel <hd4niel@users.noreply.github.com>
2020-06-09 09:12:32 +0000

    deploy: 40ef4efb2870489f3055a0ed55b1dee61690eb2a

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -3,7 +3,7 @@ var DOCUMENTATION_OPTIONS = {
     VERSION: '',
     LANGUAGE: 'sv',
     COLLAPSE_INDEX: false,
-    BUILDER: 'html',
+    BUILDER: 'dirhtml',
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
