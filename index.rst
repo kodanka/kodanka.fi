2bf0ca566cfac792380156a174ed884be3a0ab15
hd4niel <daniel.holmberg97@gmail.com>
2020-06-09 13:28:26 +0300

    Update pagemods.py

--- index.rst
+++ index.rst
@@ -10,16 +10,15 @@ Ankdammen Kodar
    faq
 
 .. toctree::
-   :maxdepth: 2
+   :maxdepth: 1
    :caption: Grunder:
 
-   grunder
    latex
    1-inledning
    6_listor
 
 .. toctree::
-   :maxdepth: 2
+   :maxdepth: 1
    :caption: Bonus:
 
    latex
--- pagemods.py
+++ pagemods.py
@@ -4,62 +4,64 @@ from bs4 import BeautifulSoup
 
 bs = lambda x: BeautifulSoup(x, 'html.parser')
 
-for file in os.listdir("_build"):
+for root, dirs, files in os.walk("_build"):
 
-    if file.endswith(".html"):
+    for file in files:
 
-        filename = f"_build/{file}"
-        name = file.split(".")[0]
-        notebook = f"{name}.ipynb"
+        if file.endswith(".html"):
 
-        # Open all html files for modification
-        with open(filename, "r") as page:
-            soup = bs(page)
+            filename = f"{root}/{file}"
+            name = file.split(".")[0]
+            notebook = f"{name}.ipynb"
 
-            # Change HTML title
-            titlename = soup.find("a", attrs={"class": "current reference internal"})
-            if titlename:
-                soup.title.string = f"Kodanka - {titlename.text.strip()}"
-            else:
-                soup.title.string = "Kodanka"
+            # Open all html files for modification
+            with open(filename, "r") as page:
+                soup = bs(page)
 
-            # Translate navigation buttons to swedish
-            next_button = soup.find("a", attrs={"accesskey": "n"})
-            if next_button:
-                next_button.clear()
-                next_button.insert(0, "Nästa")
-                next_arrow = soup.new_tag("span", attrs={"class": "fa fa-arrow-circle-right"})
-                next_button.insert(1, next_arrow)
-            previous_button = soup.find("a", attrs={"accesskey": "p"})
-            if previous_button:
-                previous_button.clear()
-                previous_button.insert(1, "Föregående")
-                previous_arrow = soup.new_tag("span", attrs={"class": "fa fa-arrow-circle-left"})
-                previous_button.insert(0, previous_arrow)
+                # Change HTML title
+                titlename = soup.find("a", attrs={"class": "current reference internal"})
+                if titlename:
+                    soup.title.string = f"Kodanka - {titlename.text.strip()}"
+                else:
+                    soup.title.string = "Kodanka"
 
-            # Translate search functionality to swedish
-            soup = bs(str(soup).replace("placeholder=\"Search docs\"", "placeholder=\"Sök\""))
+                # Translate navigation buttons to swedish
+                next_button = soup.find("a", attrs={"accesskey": "n"})
+                if next_button:
+                    next_button.clear()
+                    next_button.insert(0, "Nästa")
+                    next_arrow = soup.new_tag("span", attrs={"class": "fa fa-arrow-circle-right"})
+                    next_button.insert(1, next_arrow)
+                previous_button = soup.find("a", attrs={"accesskey": "p"})
+                if previous_button:
+                    previous_button.clear()
+                    previous_button.insert(1, "Föregående")
+                    previous_arrow = soup.new_tag("span", attrs={"class": "fa fa-arrow-circle-left"})
+                    previous_button.insert(0, previous_arrow)
 
-            # Modify page footer
-            old_footer = "Built with <a href=\"http://sphinx-doc.org/\">Sphinx</a> using a <a href=\"https://github.com/rtfd/sphinx_rtd_theme\">theme</a> provided by <a href=\"https://readthedocs.org\">Read the Docs</a>."
-            new_footer = "Byggt med <a href=\"http://sphinx-doc.org/\">Sphinx</a> med ett <a href=\"https://github.com/rtfd/sphinx_rtd_theme\">tema</a> av <a href=\"https://readthedocs.org\">Read the Docs.</a>"
-            soup = bs(str(soup).replace(old_footer, new_footer))
+                # Translate search functionality to swedish
+                soup = bs(str(soup).replace("placeholder=\"Search docs\"", "placeholder=\"Sök\""))
 
-            # Modify page header
-            if os.path.isfile(f"{name}.ipynb"):
-                colab = bs(f"<a target=\"_blank\" rel=\"noopener noreferrer\" \
-                             href=\"https://colab.research.google.com/github/kodanka/kodanka.fi/blob/master/{notebook}\"> \
-                             <img alt=\"Öppna i Colab\" src=\"_static/colab-badge.svg\" style=\"width:117px;height:20px;\"/></a>")
-                wy_list = soup.find("ul", attrs={"class": "wy-breadcrumbs"}).find_all("li")
-                if wy_list:
-                    for li in wy_list:
-                        li.decompose()
-                    soup.find("ul", attrs={"class": "wy-breadcrumbs"}).insert(0, colab)
-            else:
-                header = soup.find("div", attrs={"aria-label": "breadcrumbs navigation"})
-                if header:
-                    header.decompose()
+                # Modify page footer
+                old_footer = "Built with <a href=\"http://sphinx-doc.org/\">Sphinx</a> using a <a href=\"https://github.com/rtfd/sphinx_rtd_theme\">theme</a> provided by <a href=\"https://readthedocs.org\">Read the Docs</a>."
+                new_footer = "Byggt med <a href=\"http://sphinx-doc.org/\">Sphinx</a> med ett <a href=\"https://github.com/rtfd/sphinx_rtd_theme\">tema</a> av <a href=\"https://readthedocs.org\">Read the Docs.</a>"
+                soup = bs(str(soup).replace(old_footer, new_footer))
 
-        with open(filename, "w") as page:
-            # Write changes
-            page.write(str(soup.prettify()))
+                # Modify page header
+                if os.path.isfile(f"{name}.ipynb"):
+                    colab = bs(f"<a target=\"_blank\" rel=\"noopener noreferrer\" \
+                                href=\"https://colab.research.google.com/github/kodanka/kodanka.fi/blob/master/{notebook}\"> \
+                                <img alt=\"Öppna i Colab\" src=\"_static/colab-badge.svg\" style=\"width:117px;height:20px;\"/></a>")
+                    wy_list = soup.find("ul", attrs={"class": "wy-breadcrumbs"}).find_all("li")
+                    if wy_list:
+                        for li in wy_list:
+                            li.decompose()
+                        soup.find("ul", attrs={"class": "wy-breadcrumbs"}).insert(0, colab)
+                else:
+                    header = soup.find("div", attrs={"aria-label": "breadcrumbs navigation"})
+                    if header:
+                        header.decompose()
+
+            with open(filename, "w") as page:
+                # Write changes
+                page.write(str(soup.prettify()))
